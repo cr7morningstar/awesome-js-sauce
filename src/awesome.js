e1d9cd783cc999b391131ded60a4cc96553cf72a
IamJavid <cavidisgender@gmail.com>
2026-06-25 14:37:01 +0200

    added my comment

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,6 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+    console.log('it is 2026/25/6, I am learning Git through your repo!');
   }
 }
